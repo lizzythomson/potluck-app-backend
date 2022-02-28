e0943f6a86d0715dd4ab12ac7dab7e7a12bb20ff
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-28 16:20:35 -0700

    created tests and functionality for tests 4-10

--- src/__tests__/server.test.js
+++ src/__tests__/server.test.js
@@ -53,3 +53,61 @@ describe('test the usersModel', () => {
     expect(result[0]).toHaveProperty('username', 'paulSwift');
   });
 });
+
+describe('authentication', () => {
+  describe('[POST] /api/auth/register', () => {
+    test('[4] responds with correct message on empty body', async () => {
+      const result = await request(server)
+        .post('/api/auth/register')
+        .send({ username: '', password: '' });
+      expect(result.body.message).toMatch(/username and password required/i);
+    });
+    test('[5] responds with correct message on empty username', async () => {
+      const result = await request(server)
+        .post('/api/auth/register')
+        .send({ username: '', password: '12345' });
+      expect(result.body.message).toMatch(/username and password required/i);
+    });
+    test('[6] responds with correct message on empty password', async () => {
+      const result = await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'sammy', password: '' });
+      expect(result.body.message).toMatch(/username and password required/i);
+    });
+    test('[7] responds with the correct message on valid credentials', async () => {
+      const result = await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'sara', password: '1234' });
+      expect(result.body).toHaveProperty('username', 'sara');
+    });
+  });
+  describe('[POST] /api/auth/login', () => {
+    test('[8] responds with correct message on valid credentials', async () => {
+      await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'cammie', password: '1234' });
+      const result = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'cammie', password: '1234' });
+      expect(result.body.message).toMatch(/welcome, cammie/i);
+    });
+    test('[9] responds with correct message on invalid password', async () => {
+      await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'jenny', password: '1234' });
+      const result = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'jenny', password: '8765' });
+      expect(result.body.message).toMatch(/invalid credentials/i);
+    });
+    test('[10] responds with correct message on invalid username', async () => {
+      await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'ally', password: '1234' });
+      const result = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'allison', password: '1234' });
+      expect(result.body.message).toMatch(/invalid credentials/i);
+    });
+  });
+});
